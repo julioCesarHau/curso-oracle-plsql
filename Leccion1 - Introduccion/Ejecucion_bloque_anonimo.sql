8c73a0b864b8e2d4192b2a3245aa62e927b60ece
julioCesarHau <93216407+julioCesarHau@users.noreply.github.com>
2024-01-03 19:40:28 -0600

    Se agrega la leccion1 con el ejemplo de ejecucion de un bloque anonimo

---
+++ Leccion1 - Introduccion/Ejecucion_bloque_anonimo.sql
@@ -0,0 +1,11 @@
+--Creacion de un bloque anonimo
+SET SERVEROUTPUT ON
+DECLARE
+    l_name VARCHAR2(100);
+BEGIN
+    SELECT first_name||' '||last_name
+        INTO l_name
+    FROM employees WHERE employee_id = 100;
+    
+    DBMS_OUTPUT.put_line('El nombre del empleado es: '||l_name);    --Imprime el resultado que se guarda en la variable
+END;
