d7f97a84101679ab5951f18817365985ef82daa8
julioCesarHau <93216407+julioCesarHau@users.noreply.github.com>
2024-01-04 21:24:47 -0600

    Ejemplo1 declarando una variable

--- Leccion2 - Declarando variables/declaracion_variables.sql
+++ Leccion2 - Declarando variables/declaracion_variables.sql
@@ -1,3 +1,13 @@
 --Declarando variables
 /* Las variables deben tener una longitud maxima de 30 caracteres */
-
+--  ** Ejemplo1 **
+SET SERVEROUTPUT ON
+DECLARE
+    l_nombre VARCHAR2(30);
+BEGIN
+    DBMS_OUTPUT.put_line('Mi nombre es: '||l_nombre); --Imprime vacio ya que la variable no tiene asignado un resultado
+    l_nombre := 'Julio Cesar';
+    DBMS_OUTPUT.put_line('Mi nombre es: '||l_nombre); --imprime el nombre que se le asgino en la variable l_nombre
+END;
+/
+--  ** Ejemplo2 **
