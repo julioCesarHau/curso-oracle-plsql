67d014e9cb6b064cdd4b6c6212817dd789fbe9db
julioCesarHau <93216407+julioCesarHau@users.noreply.github.com>
2024-01-04 21:43:13 -0600

    Se modifican los comentarios en la seccion de salida del ejemplo2

--- Leccion2 - Declarando variables/declaracion_variables.sql
+++ Leccion2 - Declarando variables/declaracion_variables.sql
@@ -15,9 +15,11 @@ SET SERVEROUTPUT ON
 DECLARE
     l_nombre VARCHAR2(25) := 'Julio Cesar';
 BEGIN
-    DBMS_OUTPUT.put_line('Mi nombre es: '||l_nombre); --Imprime vacio ya que la variable no tiene asignado un resultado
+    DBMS_OUTPUT.put_line('Mi nombre es: '||l_nombre); --Imprime el nombre que está asignado a la variable en la seccion de inicializacion
     l_nombre := 'Joaquin de Jesus';
-    DBMS_OUTPUT.put_line('Mi nombre es: '||l_nombre); --imprime el nombre que se le asgino en la variable l_nombre
+    DBMS_OUTPUT.put_line('Mi nombre es: '||l_nombre); --imprime el nombre que está asignado a la variable en la seccion de ejecucion
     
     /* Aunque la variable esta inicializada en la seccion de declaracion, Siempre tomara el resultado de la ultima asignacion*/
 END;
+/
+--  ** Ejemplo3 **
